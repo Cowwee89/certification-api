3ee94e675fbbd5cfdc09ee5bd90c7cb832a7089a
Cowwee90 <101339470+Cowwee90@users.noreply.github.com>
2023-07-18 17:31:29 +0800

    endpoints for events and testresults

--- database.js
+++ database.js
@@ -7,29 +7,83 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    timezone: 'Z'
 }).promise()
 
 
 export async function getStudents() {
-    const [rows] = await pool.query("SELECT * FROM students")
+    const [rows] = await pool.query("SELECT * FROM student")
     return rows
 }
 
 export async function getStudent(id) {
     const [row] = await pool.query(`
         SELECT *
-        FROM students
+        FROM student
         WHERE id = ?
     `, [id])
     return row[0]
 }
 
-export async function createStudent(name, level) {
+export async function createStudent(sname, birthday) {
     const [result] = await pool.query(`
-        INSERT INTO students (name, level)
+        INSERT INTO student (sname, birthday)
         VALUES (?, ?)
-    `, [name, level])
+    `, [sname, birthday])
+    const id = result.insertId
+    return getStudent(id)
+}
+
+
+export async function getEvents() {
+    const [rows] = await pool.query('SELECT * FROM event')
+    return rows
+}
+
+export async function getEvent(id) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM event
+        WHERE id = ?
+    `, [id])
+    return row[0]
+}
+
+export async function createEvent(ename, edate) {
+    const [result] = await pool.query(`
+        INSERT INTO event (ename, edate)
+        VALUES (?, ?)
+    `, [ename, edate])
+    const id = result.insertId
+    return getStudent(id)
+}
+
+
+export async function getTestResults() {
+    const [rows] = await pool.query('SELECT * FROM test_result')
+    return rows
+}
+
+export async function getTestResult(id) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM test_result
+        WHERE id = ?
+    `, [id])
+    return row[0]
+}
+
+export async function createTestResult(sid, eid, solve_1, solve_2, solve_3, solve_4, solve_5, average_of_5, 
+    level_attempted, level_achieved, grade_achieved, name_to_be_printed) {
+
+    const [result] = await pool.query(`
+        INSERT INTO test_result (sid, eid, solve_1, solve_2, solve_3, solve_4, solve_5, average_of_5, 
+            level_attempted, level_achieved, grade_achieved, name_to_be_printed)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `, [sid, eid, solve_1, solve_2, solve_3, solve_4, solve_5, average_of_5, 
+        level_attempted, level_achieved, grade_achieved, name_to_be_printed])
+        
     const id = result.insertId
     return getStudent(id)
 }
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from 'express'
 
-import { getStudents, getStudent, createStudent } from './database.js'
+import { 
+    getStudents, 
+    getStudent, 
+    createStudent,
+    getEvents,
+    getEvent,
+    createEvent,
+    getTestResults,
+    getTestResult,
+    createTestResult
+} from './database.js'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
@@ -16,7 +26,6 @@ app.get('/', async (req, res) => {
 })
 
 app.get("/students", async (req, res) => {
-    console.log("getting students")
     const students = await getStudents()
     res.send(students)
 })
@@ -32,6 +41,46 @@ app.post("/students", async (req, res) => {
     const student = await createStudent(name, level)
     res.status(201).send(student)
 })
+
+
+app.get("/events", async (req, res) => {
+    const events = await getEvents()
+    res.send(events)
+})
+
+app.get("/event/:id", async (req, res) => {
+    const id = req.params.id
+    const event = await getEvent(id)
+    res.send(event)
+})
+
+app.post("/events", async (req, res) => {
+    const { name, level } = req.body
+    const event = await createEvent(name, level)
+    res.status(201).send(event)
+})
+
+
+app.get("/testresults", async (req, res) => {
+    const testresults = await getTestResults()
+    res.send(testresults)
+})
+
+app.get("/testresult/:id", async (req, res) => {
+    const id = req.params.id
+    const testresult = await getTestResult(id)
+    res.send(testresult)
+})
+
+app.post("/testresults", async (req, res) => {
+    const { sid, eid, solve_1, solve_2, solve_3, solve_4, solve_5, average_of_5, 
+        level_attempted, level_achieved, grade_achieved, name_to_be_printed } = req.body
+        
+    const testresults = await createTestResult(sid, eid, solve_1, solve_2, solve_3, solve_4, solve_5, average_of_5, 
+        level_attempted, level_achieved, grade_achieved, name_to_be_printed)
+    res.status(201).send(testresults)
+})
+
 
 app.use((err, req, res, next) => {
     console.log("error occured")
